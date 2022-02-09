888dfeee7a3d251fd7a2a0495d88bd33fa72099c
Charlie Fordon <fordoncf@gmail.com>
2022-02-09 17:41:58 -0500

    game can now be won.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,8 @@
 # hangman game
 class Hangman
-  def initialize 
+  attr_accessor :random_word
+
+  def initialize
     @word_options_array = File.read('google-10000-english-no-swears.txt').split.delete_if { |i| i.length < 5 || i.length > 12 }
     @random_word = @word_options_array.sample
     @game_over = false
@@ -10,15 +12,11 @@ class Hangman
     @game_over = true
   end
 
-  def random_word
-    @random_word
-  end
-
   def new_random_word
     @random_word = @word_options_array.sample
   end
 
-  def if_valid_guess(guess)
+  def valid_guess?(guess)
     if guess.downcase == 'save'
       puts 'gameover'
       end_game
@@ -31,20 +29,29 @@ class Hangman
     end
   end
 
+  def guess_correct?(guess, word_array)
+    true if word_array.any?(guess)
+  end
+
+  def update_word_builder(guess, word_array, word_builder)
+    word_array.each_index { |i| word_builder[i] = guess if word_array[i] == guess }
+    word_builder
+  end
+
   def game_loop
     incorrect_guess_array = []
-    word_builder = []
+    word_builder = Array.new(@random_word.length, '_')
     word_array = @random_word.split('')
-    until word_array == word_builder || incorrect_guess_array.length > 3 || @game_over == true do
+    until word_array == word_builder || incorrect_guess_array.length > 2 || @game_over == true do
       puts 'Please guess a letter.'
       guess = gets.strip
-      if if_valid_guess(guess)
-        if word_array.any?(guess)
-          puts 'present'
+      if valid_guess?(guess)
+        if guess_correct?(guess, word_array)
+          puts 'Guess is correct.'
+          word_builder = update_word_builder(guess, word_array, word_builder)
+          puts word_builder.join('')
         else
-          incorrect_guess_array.push(guess)
-          puts 'not present'
-          puts incorrect_guess_array
+          puts 'Guess is not correct.'
         end
       end
     end
