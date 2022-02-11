0cd11d8837af472e73af7a9f0ae0d0e551cc5780
Charlie Fordon <fordoncf@gmail.com>
2022-02-11 16:58:08 -0500

    game working. need save function

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,6 +6,14 @@ class Hangman
     @word_options_array = File.read('google-10000-english-no-swears.txt').split.delete_if { |i| i.length < 5 || i.length > 12 }
     @random_word = @word_options_array.sample
     @game_over = false
+    @acceptable_guess = ('a'..'z').to_a
+  end
+
+  def welcome
+    puts 'Welcome to hangman. In order to win you must
+guess a randomly selected word within 10 turns.
+You will guess one letter at a time until either you
+know the word or you run out of guesses. Good luck!'
   end
 
   def end_game
@@ -18,11 +26,10 @@ class Hangman
 
   def valid_guess?(guess)
     if guess.downcase == 'save'
-      puts 'gameover'
       end_game
       false
-    elsif guess.length > 1
-      puts 'guess must be a single letter or "save".'
+    elsif guess.length > 1 || @acceptable_guess.any?(guess.downcase) == false
+      puts 'WARNING: Guess must be a single letter or "save".'
       false
     else
       true
@@ -38,23 +45,48 @@ class Hangman
     word_builder
   end
 
+  def win_or_lose(word_array, word_builder, incorrect_guess_array)
+    if word_array == word_builder
+      puts 'You win! Congrats!'
+    elsif incorrect_guess_array.length > 9
+      puts "Better luck nect time! The correct word was '#{@random_word}'."
+    else
+      puts 'See you next time!'
+    end
+  end
+
+  def game_info(incorrect_guess_array, word_builder)
+    puts "2) Past guesses: #{incorrect_guess_array.sort.join(':')}"
+    puts "3) You have #{10 - incorrect_guess_array.length} guesses left."
+    puts "4) #{word_builder.join('')}"
+  end
+
+  def correct?(guess, word_array, word_builder, incorrect_guess_array)
+    if guess_correct?(guess, word_array)
+      puts '1) Guess is correct.'
+      word_builder = update_word_builder(guess, word_array, word_builder)
+    elsif incorrect_guess_array.any?(guess)
+      puts "1) You have already guessed #{guess}"
+    else
+      incorrect_guess_array.push(guess)
+      puts '1) Guess is not correct.'
+    end
+  end
+
   def game_loop
     incorrect_guess_array = []
     word_builder = Array.new(@random_word.length, '_')
     word_array = @random_word.split('')
-    until word_array == word_builder || incorrect_guess_array.length > 2 || @game_over == true do
+    welcome
+    until word_array == word_builder || incorrect_guess_array.length > 9 || @game_over == true do
       puts 'Please guess a letter.'
       guess = gets.strip
       if valid_guess?(guess)
-        if guess_correct?(guess, word_array)
-          puts 'Guess is correct.'
-          word_builder = update_word_builder(guess, word_array, word_builder)
-          puts word_builder.join('')
-        else
-          puts 'Guess is not correct.'
-        end
+        correct?(guess, word_array, word_builder, incorrect_guess_array)
+        game_info(incorrect_guess_array, word_builder)
       end
     end
+    win_or_lose(word_array, word_builder, incorrect_guess_array)
   end
 end
 
